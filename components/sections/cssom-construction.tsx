03b1c6d249e8b6a17fd85f418ad9138df2b8d1e3
Sean Stephenson <sean.m.stephenson@disney.com>
2026-01-04 16:44:10 -0800

    Add placeholder CSSOM section component

---
+++ components/sections/cssom-construction.tsx
@@ -0,0 +1,8 @@
+export default function CssomConstruction() {
+  return (
+    <section>
+      <h2>CSSOM Construction</h2>
+      <p>Coming soon: How browsers build the CSS Object Model</p>
+    </section>
+  );
+}
